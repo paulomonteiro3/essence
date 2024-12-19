05b8197ea2fc81ce4ab44f103c4d000bcf009f17
Paulo Monteiro <paulomonteirop22@gmail.com>
2024-12-19 11:27:46 -0300

    estrutura inicial criada

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './Componentes/Global/Header'
+import "./App.css"
+import "./Componentes/Global/Cores.css"
+import "./Componentes/Global/Fontes.css"
+
+
+const App = () => {
+  return (
+    <div>
+      <Header/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Componentes/introducao/Introducao.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Introducao = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Introducao
